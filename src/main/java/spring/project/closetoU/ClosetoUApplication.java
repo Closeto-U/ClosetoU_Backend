43e36da59d6aafd072c16c54137e2ca1bce5f640
SubAkBa <circle5926@gmail.com>
2020-12-07 23:58:35 +0900

    init commit

---
+++ src/main/java/spring/project/closetoU/ClosetoUApplication.java
@@ -0,0 +1,13 @@
+package spring.project.closetoU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetoUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetoUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/project/closetoU/ClosetoUApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.project.closetoU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetoUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
